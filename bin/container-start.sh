0eaff384304d783b05222f1142149d6cbf9e7209
Jon Hinks <projects.ws@jondh.me.uk>
2017-05-09 15:32:24 +0100

    Fix space consistency in container start script

--- bin/container-start.sh
+++ bin/container-start.sh
@@ -3,6 +3,6 @@
 # @todo The queue path and proxy address should probably be env vars
 
 php \
-	/var/app/bin/queue.php \
-	--queue-path /var/proximate/queue \
-	--proxy-address proximate-proxy:8081
+    /var/app/bin/queue.php \
+    --queue-path /var/proximate/queue \
+    --proxy-address proximate-proxy:8081
